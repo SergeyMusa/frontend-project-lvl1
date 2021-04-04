c86b6b091ab16ecb4802788097091138658bf445
SergeyMusa <sir.stv@gmail.com>
2021-04-04 22:23:50 +0300

    lesson 3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
 console.log('Welcome to the Brain Games!');
+
+import readlineSync from "readline-sync";
+import Greeting from "../src/cli.js";
+
+  const nameYou = readlineSync.question('May I have your name?: ');
+   let name = Greeting(nameYou); 
+
+/*
+//const brainGames = () => {
+
+   console.log(rez);
+   return rez;
+};
+
+//brainGames(rez);
+ */
---
+++ src/cli.js
@@ -0,0 +1,10 @@
+//import readlineSync from 'readline-sync';
+
+// module.exports = {
+export const Greeting = (nameYou) => {
+
+        let hi = `Hello, ${nameYou}!`;
+        console.log(hi);
+        return (hi);
+}
+export default Greeting;
